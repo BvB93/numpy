ee2d96c26f6e703e48c7518a9e11bc5ce0d7f96a
Bas van Beek <b.f.van.beek@vu.nl>
2020-07-29 13:02:38 +0200

    TST: use  `assert_array_equal()` when comparing arrays of the same dtype

--- numpy/lib/tests/test_index_tricks.py
+++ numpy/lib/tests/test_index_tricks.py
@@ -265,7 +265,7 @@ class TestGrid:
         grid128c_a = mgrid[0:np.longdouble(1):3.4j]
         grid128c_b = mgrid[0:np.longdouble(1):3.4j,]
         assert_(grid128c_a.dtype == grid128c_b.dtype == np.longdouble)
-        assert_array_almost_equal(grid128c_a, grid128c_b[0])
+        assert_array_equal(grid128c_a, grid128c_b[0])
 
         # different code path for single slice
         grid64 = mgrid[0.1:0.33:0.1]
@@ -285,7 +285,7 @@ class TestGrid:
         )
 
         # Related to #16945
-        assert_array_almost_equal(
+        assert_array_equal(
             mgrid[0.1:0.3:3.3j], mgrid[0.1:0.3:3.3j,][0],
         )
 
