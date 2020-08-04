20521d2365f98f45610ea94183d77e21fad29ca0
Bas van Beek <b.f.van.beek@vu.nl>
2020-08-04 17:11:12 +0200

    TST: Added `memmap` tests

--- numpy/typing/tests/data/pass/ndarray_subclass.py
+++ numpy/typing/tests/data/pass/ndarray_subclass.py
@@ -1,3 +1,4 @@
+from tempfile import TemporaryFile
 import numpy as np
 
 A = np.recarray((1, 1), dtype=[('a', int)])
@@ -9,11 +10,32 @@ A.astype(int)
 A.byteswap()
 A.copy()
 A.view(np.recarray)
-A.view(bool, np.recarray)
-A.getfield(int)
+A.view(np.ndarray)
+A.view(bool, np.ndarray)
+A.getfield(bool)
 A.reshape(-1)
 A.transpose(0, 1)
 A.swapaxes(0, 1)
 A.flatten()
 A.ravel()
 A.squeeze(0)
+
+with TemporaryFile() as f:
+    B = np.memmap(f, dtype=int, shape=(1, 1))
+    B.setflags(write=False)
+
+    B.real
+    B.imag
+    B.astype(int)
+    B.byteswap()
+    B.copy()
+    B.view(np.memmap)
+    B.view(np.ndarray)
+    B.view(bool, np.ndarray)
+    B.getfield(bool)
+    B.reshape(-1)
+    B.transpose(0, 1)
+    B.swapaxes(0, 1)
+    B.flatten()
+    B.ravel()
+    B.squeeze(0)
--- numpy/typing/tests/data/reveal/ndarray_subclass.py
+++ numpy/typing/tests/data/reveal/ndarray_subclass.py
@@ -1,3 +1,4 @@
+from tempfile import TemporaryFile
 import numpy as np
 
 A = np.recarray((1, 1), dtype=[('a', int)])
@@ -19,3 +20,24 @@ reveal_type(A.swapaxes(0, 1))  # E: numpy.recarray
 reveal_type(A.flatten())  # E: numpy.recarray
 reveal_type(A.ravel())  # E: numpy.recarray
 reveal_type(A.squeeze(0))  # E: numpy.recarray
+
+with TemporaryFile() as f:
+    B = np.memmap(f, dtype=int, shape=(1, 1))
+    B.setflags(write=False)
+
+    reveal_type(B)  # E: numpy.memmap
+    reveal_type(B.real)  # E: numpy.memmap
+    reveal_type(B.imag)  # E: numpy.memmap
+    reveal_type(B.astype(int))  # E: numpy.memmap
+    reveal_type(B.byteswap())  # E: numpy.memmap
+    reveal_type(B.copy())  # E: numpy.memmap
+    reveal_type(B.view(np.memmap))  # E: numpy.memmap
+    reveal_type(B.view(np.ndarray))  # E: numpy.ndarray
+    reveal_type(B.view(bool, np.ndarray))  # E: numpy.ndarray
+    reveal_type(B.getfield(bool))  # E: numpy.memmap
+    reveal_type(B.reshape(-1))  # E: numpy.memmap
+    reveal_type(B.transpose(0, 1))  # E: numpy.memmap
+    reveal_type(B.swapaxes(0, 1))  # E: numpy.memmap
+    reveal_type(B.flatten())  # E: numpy.memmap
+    reveal_type(B.ravel())  # E: numpy.memmap
+    reveal_type(B.squeeze(0))  # E: numpy.memmap
