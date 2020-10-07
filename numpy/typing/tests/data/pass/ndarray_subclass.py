66205409660a1515416c3051c35f97f74f9c54e9
Bas van Beek <b.f.van.beek@vu.nl>
2020-10-07 14:41:29 +0200

    TST: Update test dtypes

--- numpy/typing/tests/data/pass/ndarray_subclass.py
+++ numpy/typing/tests/data/pass/ndarray_subclass.py
@@ -1,18 +1,18 @@
 from tempfile import TemporaryFile
 import numpy as np
 
-A = np.recarray((1, 1), dtype=[('a', int)])
+A = np.recarray((1, 1), dtype=[('a', np.float64)])
 A.setflags(write=False)
 
 A.real
 A.imag
-A.astype(int)
+A.astype(np.int64)
 A.byteswap()
 A.copy()
 A.view(np.recarray)
 A.view(np.ndarray)
-A.view(bool, np.ndarray)
-A.getfield(bool)
+A.view(np.bool_, np.ndarray)
+A.getfield(np.bool_)
 A.reshape(-1)
 A.transpose(0, 1)
 A.swapaxes(0, 1)
@@ -21,18 +21,18 @@ A.ravel()
 A.squeeze(0)
 
 with TemporaryFile() as f:
-    B = np.memmap(f, dtype=int, shape=(1, 1))
+    B = np.memmap(f, dtype=np.float64, shape=(1, 1))
     B.setflags(write=False)
 
     B.real
     B.imag
-    B.astype(int)
+    B.astype(np.int64)
     B.byteswap()
     B.copy()
     B.view(np.memmap)
     B.view(np.ndarray)
-    B.view(bool, np.ndarray)
-    B.getfield(bool)
+    B.view(np.bool_, np.ndarray)
+    B.getfield(np.bool_)
     B.reshape(-1)
     B.transpose(0, 1)
     B.swapaxes(0, 1)
--- numpy/typing/tests/data/reveal/ndarray_subclass.py
+++ numpy/typing/tests/data/reveal/ndarray_subclass.py
@@ -1,19 +1,19 @@
 from tempfile import TemporaryFile
 import numpy as np
 
-A = np.recarray((1, 1), dtype=[('a', int)])
+A = np.recarray((1, 1), dtype=[('a', np.float64)])
 A.setflags(write=False)
 
 reveal_type(A)  # E: numpy.recarray
 reveal_type(A.real)  # E: numpy.recarray
 reveal_type(A.imag)  # E: numpy.recarray
-reveal_type(A.astype(int))  # E: numpy.recarray
+reveal_type(A.astype(np.int64))  # E: numpy.recarray
 reveal_type(A.byteswap())  # E: numpy.recarray
 reveal_type(A.copy())  # E: numpy.recarray
 reveal_type(A.view(np.recarray))  # E: numpy.recarray
 reveal_type(A.view(np.ndarray))  # E: numpy.ndarray
-reveal_type(A.view(bool, np.ndarray))  # E: numpy.ndarray
-reveal_type(A.getfield(bool))  # E: numpy.recarray
+reveal_type(A.view(np.bool_, np.ndarray))  # E: numpy.ndarray
+reveal_type(A.getfield(np.bool_))  # E: numpy.recarray
 reveal_type(A.reshape(-1))  # E: numpy.recarray
 reveal_type(A.transpose(0, 1))  # E: numpy.recarray
 reveal_type(A.swapaxes(0, 1))  # E: numpy.recarray
@@ -22,19 +22,19 @@ reveal_type(A.ravel())  # E: numpy.recarray
 reveal_type(A.squeeze(0))  # E: numpy.recarray
 
 with TemporaryFile() as f:
-    B = np.memmap(f, dtype=int, shape=(1, 1))
+    B = np.memmap(f, dtype=np.float64, shape=(1, 1))
     B.setflags(write=False)
 
     reveal_type(B)  # E: numpy.memmap
     reveal_type(B.real)  # E: numpy.memmap
     reveal_type(B.imag)  # E: numpy.memmap
-    reveal_type(B.astype(int))  # E: numpy.memmap
+    reveal_type(B.astype(np.int64))  # E: numpy.memmap
     reveal_type(B.byteswap())  # E: numpy.memmap
     reveal_type(B.copy())  # E: numpy.memmap
     reveal_type(B.view(np.memmap))  # E: numpy.memmap
     reveal_type(B.view(np.ndarray))  # E: numpy.ndarray
-    reveal_type(B.view(bool, np.ndarray))  # E: numpy.ndarray
-    reveal_type(B.getfield(bool))  # E: numpy.memmap
+    reveal_type(B.view(np.bool_, np.ndarray))  # E: numpy.ndarray
+    reveal_type(B.getfield(np.bool_))  # E: numpy.memmap
     reveal_type(B.reshape(-1))  # E: numpy.memmap
     reveal_type(B.transpose(0, 1))  # E: numpy.memmap
     reveal_type(B.swapaxes(0, 1))  # E: numpy.memmap
