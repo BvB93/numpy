9e50719a1c9b3d03f79bba2cf42a8794eca10f90
Bas van Beek <b.f.van.beek@vu.nl>
2021-01-26 18:27:47 +0100

    TST: Use a `pytest` fixture for the `isfile` tests

--- numpy/typing/tests/test_isfile.py
+++ numpy/typing/tests/test_isfile.py
@@ -1,32 +1,35 @@
 import os
 from pathlib import Path
 
+import pytest
 import numpy as np
 from numpy.testing import assert_
 
 ROOT = Path(np.__file__).parents[0]
-FILES = [
-    ROOT / "py.typed",
-    ROOT / "__init__.pyi",
-    ROOT / "char.pyi",
-    ROOT / "ctypeslib.pyi",
-    ROOT / "rec.pyi",
-    ROOT / "core" / "__init__.pyi",
-    ROOT / "distutils" / "__init__.pyi",
-    ROOT / "f2py" / "__init__.pyi",
-    ROOT / "fft" / "__init__.pyi",
-    ROOT / "lib" / "__init__.pyi",
-    ROOT / "linalg" / "__init__.pyi",
-    ROOT / "ma" / "__init__.pyi",
-    ROOT / "matrixlib" / "__init__.pyi",
-    ROOT / "polynomial" / "__init__.pyi",
-    ROOT / "random" / "__init__.pyi",
-    ROOT / "testing" / "__init__.pyi",
-]
 
 
-class TestIsFile:
-    def test_isfile(self):
-        """Test if all ``.pyi`` files are properly installed."""
-        for file in FILES:
-            assert_(os.path.isfile(file))
+@pytest.mark.parametrize(
+    "file",
+    [
+        ROOT / "py.typed",
+        ROOT / "__init__.pyi",
+        ROOT / "char.pyi",
+        ROOT / "ctypeslib.pyi",
+        ROOT / "rec.pyi",
+        ROOT / "core" / "__init__.pyi",
+        ROOT / "distutils" / "__init__.pyi",
+        ROOT / "f2py" / "__init__.pyi",
+        ROOT / "fft" / "__init__.pyi",
+        ROOT / "lib" / "__init__.pyi",
+        ROOT / "linalg" / "__init__.pyi",
+        ROOT / "ma" / "__init__.pyi",
+        ROOT / "matrixlib" / "__init__.pyi",
+        ROOT / "polynomial" / "__init__.pyi",
+        ROOT / "random" / "__init__.pyi",
+        ROOT / "testing" / "__init__.pyi",
+        ROOT / "typing" / "_ufuncs.pyi",
+    ],
+)
+def test_isfile(file: Path) -> None:
+    """Test if all ``.pyi`` files are properly installed."""
+    assert_(os.path.isfile(file))
