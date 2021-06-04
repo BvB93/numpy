424ab2eda8050521aff98575f0ee47cb1ad470fd
Bas van Beek <b.f.van.beek@vu.nl>
2021-06-04 10:44:26 +0200

    ENH

--- numpy/lib/histograms.pyi
+++ numpy/lib/histograms.pyi
@@ -1,7 +1,51 @@
-from typing import List
+from typing import List, Tuple, overload, Any
+
+from numpy import floating, complexfloating, object_
+from numpy.typing import (
+    NDArray,
+    ArrayLike,
+    _ArrayLikeInt_co,
+    _ArrayLikeFloat_co,
+    _ArrayLikeComplex_co,
+    _ArrayLikeObject_co,
+)
+
+from typing_extensions import Literal as L, SupportsIndex
+
+_BinKind = L[
+    "stone",
+    "auto",
+    "doane",
+    "fd",
+    "rice",
+    "scott",
+    "sqrt",
+    "sturges",
+]
 
 __all__: List[str]
 
-def histogram_bin_edges(a, bins=..., range=..., weights=...): ...
+@overload
+def histogram_bin_edges(  # type: ignore[misc]
+    a: _ArrayLikeFloat_co,
+    bins: _BinKind | SupportsIndex | _ArrayLikeFloat_co = ...,
+    range: None | Tuple[float, float] = ...,
+    weights: None | ArrayLike = ...,
+) -> NDArray[floating[Any]]: ...
+@overload
+def histogram_bin_edges(
+    a: _ArrayLikeComplex_co,
+    bins: _BinKind | SupportsIndex | _ArrayLikeComplex_co = ...,
+    range: None | Tuple[float, float] = ...,
+    weights: None | ArrayLike = ...,
+) -> NDArray[complexfloating[Any, Any]]: ...
+@overload
+def histogram_bin_edges(
+    a: _ArrayLikeObject_co,
+    bins: _BinKind | SupportsIndex | Any = ...,
+    range: None | Tuple[float, float] = ...,
+    weights: None | ArrayLike = ...,
+) -> NDArray[object_]: ...
+
 def histogram(a, bins=..., range=..., normed=..., weights=..., density=...): ...
 def histogramdd(sample, bins=..., range=..., normed=..., weights=..., density=...): ...
